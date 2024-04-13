bd48464f5ae24ab99846a8897e363ca9f54c86fd
sanket mane <sanketmmane@gmail.com>
2024-04-13 11:51:12 +0530

    basic types

---
+++ src/01_basictype.ts
@@ -0,0 +1,6 @@
+let id: number = 5;
+let company: string = "vscode";
+// company = 10 :Type 'number' is not assignable to type 'string'
+let isPublished: boolean = true;
+let x: any = "hello"; //The any type in TypeScript is a workaround for cases when you don't know what type a value might be
+
